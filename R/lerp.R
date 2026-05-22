daaadf77afdaa17631a810e15c4db72ea09c6592
paithiov909 <am11.44@gmail.com>
2026-05-23 01:37:09 +0900

    tweaks

--- R/lerp.R
+++ R/lerp.R
@@ -42,7 +42,9 @@ lerp.character <- function(x, y, t) {
 #' @export
 lerp.nativeRaster <- function(x, y, t) {
   out <- lerp_cpp(as.integer(x), as.integer(y), t)
-  dim(out) <- dim(x)
-  class(out) <- unique(c("nativeRaster", class(out)))
-  out
+  structure(
+    out,
+    class = unique(c("nativeRaster", class(out))),
+    dim = dim(x)
+  )
 }
